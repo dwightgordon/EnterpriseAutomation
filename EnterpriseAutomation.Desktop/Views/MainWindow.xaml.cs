7f9dca38b33289a62230e9319c47f52068e0d4ae
Administrator <Administrator@COBRA>
2026-03-01 08:55:13 +0100

    Initial WPF .NET 8 foundation with DI configured

---
+++ EnterpriseAutomation.Desktop/Views/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using EnterpriseAutomation.Desktop.Views;
+
+namespace EnterpriseAutomation.Desktop.Views
+{
+    public partial class App : Application
+    {
+        public static IHost AppHost { get; private set; }
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((Context, Services) =>
+                {
+                    Services.AddSingleton<MainWindow>();
+                })
+                .Build();
+        }
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost.StartAsync();
+            var mainWindow = AppHost.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost.StopAsync();
+            AppHost.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
